16ab2595becf2b69f38e22eda66dbe32d0673227
ferrarapeppe <ferrarapeppe@users.noreply.github.com>
2026-03-30 12:05:28 +0000

    deploy: 062b5e40174de0b4435e6037e0b12cb8c747c8bb

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -34,9 +34,9 @@ const RESOURCES = {"manifest.json": "2d43a3998fb27d5b655493dd358f6863",
 "assets/fonts/MaterialIcons-Regular.otf": "c68d84eb071a0831a26be3ca8f5f4ab8",
 "assets/AssetManifest.bin.json": "5d3ee0b8555878a8bed727fcd5f4a3f6",
 "booking.html": "46f99ab4e86400354fe8cf08aef43bca",
-"flutter_bootstrap.js": "764ac314c53f2839945567c49fbcdda9",
+"flutter_bootstrap.js": "6cf3f6bd2426109aadd310dd9b556879",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
-"main.dart.js": "ea31a752d5c71cf1c30fd832a7db75d5"};
+"main.dart.js": "5d3e4a040c71c5cebb580fe5b6f2e47f"};
 // The application shell files that are downloaded before a service worker can
 // start.
 const CORE = ["main.dart.js",
