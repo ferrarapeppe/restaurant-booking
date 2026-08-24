935071608a3c05e69972d6d802a994745628825b
ferrarapeppe <ferrarapeppe@users.noreply.github.com>
2026-08-24 13:31:33 +0000

    deploy: bd5acb20b28ae0d0ccfbad1c3c10eb25b5d02d49

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"82bd5b7209295a5b7ff8cae0df96e7870171e3
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "2531308266"
+    serviceWorkerVersion: "3703878053"
   }
 });
