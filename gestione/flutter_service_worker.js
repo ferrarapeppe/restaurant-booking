2a47edabfc84aec4153efae97e7633f54022f549
ferrarapeppe <ferrarapeppe@users.noreply.github.com>
2026-08-27 10:20:18 +0000

    deploy: 32921abc0b5fb072dcceaa75f908f0e0932fd454

--- gestione/flutter_service_worker.js
+++ gestione/flutter_service_worker.js
@@ -34,10 +34,10 @@ const RESOURCES = {"version.json": "dca05162d8cc58448747c8e00c237df8",
 "assets/assets/images/logo_drawer.png": "fb81200e3012ec96704b432fb3bcf355",
 "assets/assets/images/logo_splash.png": "2383e2b745fdb683e09edf82c99b32a0",
 "assets/shaders/ink_sparkle.frag": "ecc85a2e95f5e9f53123dcaf8cb9b6ce",
-"main.dart.js": "ddf52bea8f2dba49ebcda6e1b95b50fc",
+"main.dart.js": "6dde936a55f8daba13e2d4d0d3879ef8",
 "logo.png": "f58f36a5482f86e1e8555ccc72e9746a",
 "booking.html": "c987c694b8cd14c545e55c3cf090a884",
-"flutter_bootstrap.js": "2bc894634b7a531da8291d26aa3bb392",
+"flutter_bootstrap.js": "3ee1dd0d59e610dc64cdb2b7da497de8",
 "manifest.json": "47c62533783d380647e8bb69554b7aeb"};
 // The application shell files that are downloaded before a service worker can
 // start.
